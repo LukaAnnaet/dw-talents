01c10706cc9218f61b31affd1b786f20aa7a2737
LukaAnnaet <lukaannaet@gmail.com>
2024-10-18 15:34:00 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,6 +1,17 @@
 // index.js
-import GetAPIObjects from './GetAPIObjects.js';
-import talentTable from './Talents.js';
+import getData from './getData.js';
+import talentTable from './table.js';
+
+function createTable(apiData, resultDiv) {
+    resultDiv.innerHTML = talentTable(apiData);
+    if ($('#result-table').length) $('#result-table').DataTable();
+}
+
+function displayErrorMessage(message) {
+    const errorDiv = document.getElementById('error-message');
+    errorDiv.textContent = message;
+    errorDiv.style.display = 'block';
+}
 
 document.getElementById('api-buttons').addEventListener('click', async (event) => {
     if (event.target.tagName !== 'BUTTON') return;
@@ -9,14 +20,16 @@ document.getElementById('api-buttons').addEventListener('click', async (event) =
     const resultDiv = document.getElementById('result');
 
     try {
-        const apiData = await GetAPIObjects[actionMethod]();
+        const apiData = await getData.fetchData(actionMethod);
+
         if (actionMethod === 'getAllAPITalents' && event.target.id === 'fetch-talents') {
-            resultDiv.innerHTML = talentTable(apiData);
-        } else{
+            createTable(apiData, resultDiv);
+        } else {
             resultDiv.innerHTML = `<pre>${JSON.stringify(apiData, null, 2)}</pre>`;
         }
+
     } catch (error) {
         console.error('Error:', error);
-        resultDiv.innerHTML = '<p style="color: red;">An error occurred while fetching data. Please try again later.</p>';
+        displayErrorMessage(`Failed to fetch data for ${actionMethod}. Please try again later.`);
     }
 });
